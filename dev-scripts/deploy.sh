86a915089d61a9d4b80669f223594396532f9f60
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-05-12 13:46:20 +0300

    OPH-492: Lisätään dev-avain ssh-addilla ennen Vagrantiin julkaisua

--- dev-scripts/deploy.sh
+++ dev-scripts/deploy.sh
@@ -18,4 +18,6 @@ cd $repo_path/aipal-db
 ./build.sh
 
 cd $repo_path/ansible
+chmod 600 yhteiset/dev_id_rsa
+ssh-add yhteiset/dev_id_rsa
 ansible-playbook -v -i aipal_vagrant/hosts yhteiset/julkaise_paikallinen_versio.yml -e "sovellus_jar=\"$repo_path/aipal/target/aipal-standalone.jar\" migraatio_jar=\"$repo_path/aipal-db/target/aipal-db-standalone.jar\""
