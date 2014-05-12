c1b75ed17304d123ada484cd00140688c99dd305
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-05-12 14:53:46 +0300

    OPH-492: app-server.sh: Ei tehdä mitään, minkä asennusskripti voi tehdä

--- env/app-server.sh
+++ env/app-server.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -eu
 
-if [ $# -lt 3 ]
+if [ $# -ne 2 ]
 then
-    echo "$0 <ympäristön-asetusten-polku> <tietokantapalvelimen-ip> <deploy_id_rsa.pub> [base-url]"
+    echo "$0 <ympäristön-asetusten-polku> <deploy_id_rsa.pub>"
     exit 1
 fi
 
 system='aipal'
 
-ip=`hostname -I | cut -f1 -d' '`
 env_dir=$1
-db_host=$2
-id_rsa_pub=$3
-base_url=${4:-"http://$ip/$system"}
-install_dir=/data00/$system
-install_jar=$system.jar
+id_rsa_pub=$2
 admin_user="${system}admin"
 
 set -x
@@ -23,35 +18,11 @@ set -x
 software/jre.sh
 software/httpd.sh
 
-mkdir -p $install_dir
-
 useradd $admin_user
-
-# Järjestelmää ajetaan tomcat-käyttäjänä
-useradd -r -s /bin/false tomcat
 
 # admin-käyttäjälle oikeudet ajaa rootina asennukseen tarvittavat komennot
 # ilman salasanaa
 echo "$admin_user ALL = NOPASSWD: ALL" >> /etc/sudoers
-
-#init.d-skripti
-cp common/app-server/$system-init.d.sh /etc/init.d/$system
-sed -i -e "s|<APP_HOME>|$install_dir|g" /etc/init.d/$system
-chmod 755 /etc/init.d/$system
-
-# Palvelimen asetukset
-cp $env_dir/app-server/$system.properties $install_dir
-mkdir "$install_dir/resources"
-chmod a+rx "$install_dir/resources"
-cp $env_dir/app-server/logback.xml "$install_dir/resources"
-mkdir "$install_dir/logs"
-chmod a+rwx "$install_dir/logs"
-sed -i -e "s|<DB_HOST>|$db_host|g" $install_dir/$system.properties
-sed -i -e "s|<BASE_URL>|$base_url|g" $install_dir/$system.properties
-
-# Migraatioiden asetukset
-cp $env_dir/app-server/$system-db.properties $install_dir
-sed -i -e "s|<DB_HOST>|$db_host|g" $install_dir/$system-db.properties
 
 # Sallitaan asennusten pääsy ssh:lla
 mkdir /home/$admin_user/.ssh
