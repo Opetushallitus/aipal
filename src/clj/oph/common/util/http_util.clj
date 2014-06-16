7dced3998340e83108e3c7ca7d473d4a29c44da9
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2014-06-16 10:29:15 +0300

    Kopioi http_util päivämäärähelpereitä aipalista.

--- src/clj/oph/common/util/http_util.clj
+++ src/clj/oph/common/util/http_util.clj
@@ -1,6 +1,8 @@
 (ns oph.common.util.http-util
   (:require
     [cheshire.core :as cheshire]
+    [clj-time.format :refer [formatter parse-local-date]]
+    [clj-time.coerce :as time-coerce]
     [schema.core :as s] ))
 
 (defn json-response
@@ -17,3 +19,27 @@
 (defn json-response-nocache
   [data]
   (assoc-in (json-response data 0) [:headers "Cache-control"] "max-age=0"))
+
+(defn convert-instances-of [c f m]
+  (clojure.walk/postwalk #(if (instance? c %) (f %) %) m))
+
+(defn joda-date->sql-date [m]
+  (convert-instances-of org.joda.time.LocalDate
+                        time-coerce/to-sql-date
+                        m))
+
+(defn try-parse-local-date
+  [f d]
+  (try
+    (joda-date->sql-date (parse-local-date (formatter f) d))
+    (catch IllegalArgumentException e
+      nil)))
+
+(defn parse-iso-date
+  [d]
+  (when d
+    (or
+      (try-parse-local-date "yyyy-MM-dd'T'HH:mm:ss.sssZ" d)
+      (try-parse-local-date "yyyy-MM-dd" d)
+      (try-parse-local-date "dd.MM.yyyy" d)
+      (throw (IllegalArgumentException. "Virheellinen pvm formaatti")))))
