86645afbff639a98b58243b42cd5a00cf2cb7f45
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-07-01 15:27:07 +0300

    Nostettu versioita

--- project.clj
+++ project.clj
@@ -15,32 +15,32 @@
 (defproject aipalvastaus "0.1.0-SNAPSHOT"
   :description "Aipalvastaus"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.1"]
-                 [http-kit "2.1.10"]
-                 [compojure "1.1.5"]
-                 [ring/ring-json "0.2.0"]
+                 [ring/ring-core "1.3.0"]
+                 [http-kit "2.1.18"]
+                 [compojure "1.1.8"]
+                 [ring/ring-json "0.3.1"]
                  [ring/ring-headers "0.1.0"]
-                 [cheshire "5.2.0"]
-                 [stencil "0.3.3"]
-                 [org.clojure/tools.logging "0.2.6"]
+                 [cheshire "5.3.1"]
+                 [stencil "0.3.4"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.slf4j/slf4j-api "1.7.5"]
-                 [clj-time "0.6.0"]
+                 [clj-time "0.7.0"]
                  [com.cemerick/valip "0.3.2"]
                  [prismatic/schema "0.2.0"]
-                 [korma "0.3.0-RC6"]
-                 [postgresql "9.1-901.jdbc4"]
-                 [stencil "0.3.2"]
+                 [korma "0.3.2"]
+                 [org.postgresql/postgresql "9.3-1101-jdbc41"]
                  [com.jolbox/bonecp "0.8.0.RELEASE"]]
   :plugins [[test2junit "1.0.1"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                                  [clj-webdriver "0.6.0"]
-                                  [clj-http "0.7.6"]
+                                  [clj-webdriver "0.6.1"]
+                                  [clj-http "0.9.2"]
                                   [ring-mock "0.1.5"]]}
              :uberjar {:main aipalvastaus.palvelin
                        :aot :all}}
   :source-paths ["src/clj"]
+  :test-paths ["test/clj"]
   :main aipalvastaus.palvelin
   :repl-options {:init-ns user}
   :jar-name "aipalvastaus.jar"
