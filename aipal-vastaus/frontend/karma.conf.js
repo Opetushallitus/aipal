b8550bc96ad4c7907f004868a62762d566482b7a
Mikko Suonio <mikko.suonio@solita.fi>
2014-08-07 14:45:29 +0300

    Otan pois Javascript-testien ajon PhantomJS:llä

--- aipal-vastaus/frontend/karma.conf.js
+++ aipal-vastaus/frontend/karma.conf.js
@@ -62,7 +62,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
