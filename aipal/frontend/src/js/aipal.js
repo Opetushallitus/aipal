5f8a028d5dbed4f6cf9eabd5e27c648a7e7cdeee
Antti Virtanen <antti.virtanen@iki.fi>
2014-08-07 13:24:47 +0300

    OPH-674 vastaajatunnusten käsittely käyttöliittymässä

--- aipal/frontend/src/js/aipal.js
+++ aipal/frontend/src/js/aipal.js
@@ -20,6 +20,7 @@ angular.module('aipal', [
     'kysely.kyselyui',
     'kyselypohja.kyselypohjaui',
     'kysymysryhma.kysymysryhmaui',
+    'vastaajatunnus.vastaajatunnusui',
     'raportti.kyselykerta.kyselykertaui',
     'yhteiset.palvelut.i18n',
     'yhteiset.palvelut.apicallinterceptor',
