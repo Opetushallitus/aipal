3569b79fe1750de2fe31c171525f6a6df13982e4
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2014-08-12 15:32:20 +0300

    OPH-665: hae-kysymysryhmat: Yksinkertaista kyselyä.

--- aipal/src/clj/aipal/arkisto/kysely.clj
+++ aipal/src/clj/aipal/arkisto/kysely.clj
@@ -89,8 +89,6 @@
     (sql/order :kysely_kysymysryhma.jarjestys)
     (sql/with kysymys
               (sql/fields :kysymysid :kysymys_fi :kysymys_sv :poistettava :pakollinen [(sql/raw "kysely_kysymys.kysymysid is null") :poistettu])
-              (sql/join :inner kysely_kysymysryhma (= :kysely_kysymysryhma.kysymysryhmaid :kysymys.kysymysryhmaid))
               (sql/join :left :kysely_kysymys (and (= :kysely_kysymys.kysymysid :kysymysid) (= :kysely_kysymys.kyselyid kyselyid)))
-              (sql/where {:kysely_kysymysryhma.kyselyid kyselyid})
               (sql/order :kysymys.jarjestys))
     sql/exec))
