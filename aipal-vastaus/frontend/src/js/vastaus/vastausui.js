d001e9f74f179ec795a5b6cf7b89a0849055aeee
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-08-12 14:33:37 +0300

    OPH-662: vastausten tallennus ui:ssa

--- aipal-vastaus/frontend/src/js/vastaus/vastausui.js
+++ aipal-vastaus/frontend/src/js/vastaus/vastausui.js
@@ -12,7 +12,23 @@ angular.module('vastaus.vastausui', ['ngRoute'])
 
   .controller('VastausController', ['$http', '$routeParams', '$scope', function($http, $routeParams, $scope) {
     $scope.tunnus = $routeParams.tunnus;
-    $scope.answers = {};
+    $scope.monivalinta = {};
+
+    $scope.tallenna = function() {
+      // TODO: vastausdatan käsittely ja lähetys
+    };
+
+    $scope.vaihdaMonivalinta = function(vaihtoehto, kysymysid) {
+      if (_.isUndefined($scope.monivalinta[kysymysid])) {
+        $scope.monivalinta[kysymysid] = 0;
+      }
+      if(vaihtoehto.valittu) {
+        $scope.monivalinta[kysymysid]++;
+      }
+      else {
+        $scope.monivalinta[kysymysid]--;
+      }
+    };
 
     $http.get('/api/kyselykerta/' + $routeParams.tunnus).success(function(data) {
       $scope.data = data;
