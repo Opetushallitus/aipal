ba9d64d19113a611ae874cc5b3a6b7b8834f995d
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-09-19 10:03:14 +0300

    OPH-838: kayttaja.middleware: Eriytetään impersonoitu-oid letiin

--- aipal/src/clj/aipal/infra/kayttaja/middleware.clj
+++ aipal/src/clj/aipal/infra/kayttaja/middleware.clj
@@ -4,9 +4,10 @@
 (defn wrap-kayttaja [handler]
   (fn [request]
     ;; CAS-middleware lisää käyttäjätunnuksen :username-avaimen alle
-    (let [uid (:username request)]
+    (let [uid (:username request)
+          impersonoitu-oid (get-in request [:session :impersonoitu-oid])]
       (try
-        (with-kayttaja uid (get-in request [:session :impersonoitu-oid])
+        (with-kayttaja uid impersonoitu-oid
           (handler request))
         (catch IllegalStateException _
           {:headers {"Content-Type" "text/plain;charset=utf-8"}
