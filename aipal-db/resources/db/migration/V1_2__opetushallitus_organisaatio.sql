2512af22d65cac6ffba617a5170072eb6bc84328
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-09-29 15:03:17 +0300

    OPH-871: Opetushallitus kantaan koulutustoimija tauluun

---
+++ aipal-db/resources/db/migration/V1_2__opetushallitus_organisaatio.sql
@@ -0,0 +1,3 @@
+-- Opetushallitukselle oma organisaatio, ei varsinainen koulutustoimija
+INSERT INTO koulutustoimija (ytunnus, nimi_fi, oid)
+VALUES ('0829731-2', 'Opetushallitus', '1.2.246.562.10.00000000001');
