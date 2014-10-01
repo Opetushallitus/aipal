f8e8ec1e8057b498d2d511b79837aa2e5e9a1375
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-10-01 15:38:24 +0300

    korjaus kysymysryhmänäkymään

--- aipal-db/resources/db/migration/V1_4__kysymysryhma_organisaatioview.sql
+++ aipal-db/resources/db/migration/V1_4__kysymysryhma_organisaatioview.sql
@@ -13,12 +13,7 @@ union all
   select
     kr.kysymysryhmaid, kr.koulutustoimija, kr.oppilaitos, kr.toimipaikka, kr.valtakunnallinen
   from kysymysryhma kr
-  where kr.koulutustoimija is not null
-union all
-  select
-    kr.kysymysryhmaid, kr.koulutustoimija, kr.oppilaitos, kr.toimipaikka, kr.valtakunnallinen
-  from kysymysryhma kr
-  where kr.valtakunnallinen = true
+  where kr.koulutustoimija is not null or kr.valtakunnallinen = true
 ;
 
 COMMENT ON VIEW kysymysryhma_organisaatio_view is 'kysymysryhmien omistaja-organisaatiot sekä valtakunnalliset kysymysryhmät.';
