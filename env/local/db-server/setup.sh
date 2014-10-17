7c664cb8fa1e08250aa38d75d909711880e1f9e5
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-10-17 10:46:09 +0300

    Luodaan vagrant kantaan peruskäyttäjät jotta migraatio onnistuu tyhjään kantaan

--- env/local/db-server/setup.sh
+++ env/local/db-server/setup.sh
@@ -4,3 +4,7 @@ set -eu
 # poistetaan esteet hostin ja guestin valilta
 iptables -F
 service iptables save
+
+# postgres käyttäjien perussetup
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+sudo -u postgres psql < $script_path/dev.sql
