65426bf3324b1e34db8712cdc75aaa15438b3321
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-10-24 14:21:45 +0300

    OPH-658: poistettu turhaa koodia

--- aipal-vastaus/src/clj/aipalvastaus/sql/korma.clj
+++ aipal-vastaus/src/clj/aipalvastaus/sql/korma.clj
@@ -1,13 +1,6 @@
 (ns aipalvastaus.sql.korma
-  (:import java.sql.Date
-           java.sql.Timestamp
-           org.joda.time.DateTime
-           org.joda.time.LocalDate)
   (:require korma.db
-            [korma.core :as sql]
-            [aipalvastaus.sql.korma-auth :as korma-auth]
-            [clj-time.coerce :as time-coerce]
-            [clj-time.core :as time]))
+            [aipalvastaus.sql.korma-auth :as korma-auth]))
 
 (defn korma-asetukset
   "Muuttaa asetustiedoston db-avaimen arvon Korman odottamaan muotoon."
@@ -37,58 +30,3 @@
     (korma.db/create-db {:make-pool? false
                          :delimiters ""
                          :datasource (bonecp-datasource db-asetukset)})))
-
-(defn convert-instances-of [c f m]
-  (clojure.walk/postwalk #(if (instance? c %) (f %) %) m))
-
-(defn joda-datetime->sql-timestamp [m]
-  (convert-instances-of org.joda.time.DateTime
-                        time-coerce/to-sql-time
-                        m))
-
-(defn sql-timestamp->joda-datetime [m]
-  (convert-instances-of java.sql.Timestamp
-                        time-coerce/from-sql-time
-                        m))
-
-(defn ^:private to-local-date-default-tz
-  [date]
-  (let [dt (time-coerce/to-date-time date)]
-    (time-coerce/to-local-date (time/to-time-zone dt (time/default-time-zone)))))
-
-(defn sql-date->joda-date [m]
-  (convert-instances-of java.sql.Date
-                        to-local-date-default-tz
-                        m))
-
-(defn joda-date->sql-date [m]
-  (convert-instances-of org.joda.time.LocalDate
-                        time-coerce/to-sql-date
-                        m))
-(defmacro defentity
-  "Wrapperi Korman defentitylle, lisää yleiset prepare/transform-funktiot."
-  [ent & body]
-  `(sql/defentity ~ent
-                  (sql/prepare joda-date->sql-date)
-                  (sql/prepare joda-datetime->sql-timestamp)
-                  (sql/transform sql-date->joda-date)
-                  (sql/transform sql-timestamp->joda-datetime)
-                  ~@body))
-
-(defn entity-alias [entity alias]
-  (assoc entity :name alias
-                :alias alias))
-
-;; Korma ei salli useampaa kuin yhtä linkkiä samojen entityjen välillä.
-;; Tämä makro tekee kopion entitystä uudelle nimelle.
-(defmacro defalias [alias entity]
-  `(def ~alias (entity-alias ~entity ~(name alias))))
-
-
-(def validationquery "select count(*) from kayttajarooli")
-
-(defn validate-connection!
-  []
-  (first
-    (sql/select validationquery)))
-
