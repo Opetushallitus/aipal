30b90aaa2ab2a08bffcf88e101d53f6af3ab4472
Mikko Suonio <mikko.suonio@solita.fi>
2014-11-05 11:10:00 +0200

    OPH-959: Siistin testejä

--- aipal/test/clj/aipal/rest_api/kysymysryhma_lisaa_kysymys_test.clj
+++ aipal/test/clj/aipal/rest_api/kysymysryhma_lisaa_kysymys_test.clj
@@ -3,9 +3,6 @@
             [korma.core :as sql]
             [aipal.rest-api.kysymysryhma :refer [lisaa-kysymys! paivita-kysymysryhma!]]))
 
-(def oletus-kysymys
-  {})
-
 (defn arkisto-stub-fixture [f]
   (with-redefs [aipal.arkisto.kysymysryhma/hae (fn [kysymysryhmaid] {})
                 aipal.arkisto.kysymysryhma/poista-kysymys! (fn [kysymysid])
@@ -19,43 +16,39 @@
 (use-fixtures :each arkisto-stub-fixture)
 
 (deftest lisaa-kysymys-lisaa-jatkokysymyksen
-  (let [kysymys (-> oletus-kysymys
-                  (assoc :vastaustyyppi "kylla_ei_valinta")
-                  (assoc :jatkokysymys {:kylla_teksti_fi "Jatkokysymys"}))
-        lisatty-jatkokysymys (atom nil)
-        lisatty-kysymys (atom nil)
-        kysymysryhmaid 11
-        uusi-jatkokysymysid 111]
-    (with-redefs [aipal.arkisto.kysymysryhma/lisaa-kysymys! (partial reset! lisatty-kysymys)
-                  aipal.arkisto.kysymysryhma/lisaa-jatkokysymys! (fn [jatkokysymys]
-                                                                   (reset! lisatty-jatkokysymys
-                                                                           (assoc jatkokysymys :jatkokysymysid uusi-jatkokysymysid)))]
-      (lisaa-kysymys! kysymys kysymysryhmaid)
-      (is (= "Jatkokysymys" (-> @lisatty-jatkokysymys
-                              :kylla_teksti_fi)))
-      (is (= uusi-jatkokysymysid (-> @lisatty-kysymys
-                                   :jatkokysymysid))))))
+  (let [lisatty-jatkokysymys (atom nil)
+        lisatty-kysymys (atom nil)]
+    (with-redefs [aipal.arkisto.kysymysryhma/lisaa-kysymys!
+                  (partial reset! lisatty-kysymys)
+
+                  aipal.arkisto.kysymysryhma/lisaa-jatkokysymys!
+                  (fn [jatkokysymys]
+                    (reset! lisatty-jatkokysymys
+                            (assoc jatkokysymys :jatkokysymysid 3)))]
+      (lisaa-kysymys! {:vastaustyyppi "kylla_ei_valinta"
+                       :jatkokysymys {:kylla_teksti_fi "Jatkokysymys"}} 1)
+      (is (= (:kylla_teksti_fi @lisatty-jatkokysymys) "Jatkokysymys"))
+      (is (= (:jatkokysymysid @lisatty-kysymys) 3)))))
 
 (deftest lisaa-kysymys-lisaa-kysymyksen
   (let [kysymys {:kysymys_fi "Kysymys"}
-        lisatty-kysymys (atom nil)
-        kysymysryhmaid 11]
+        lisatty-kysymys (atom nil)]
     (with-redefs [aipal.arkisto.kysymysryhma/lisaa-kysymys! (partial reset! lisatty-kysymys)]
-      (lisaa-kysymys! kysymys kysymysryhmaid)
-      (is (= {:kysymysryhmaid kysymysryhmaid :kysymys_fi "Kysymys"}
-             (-> @lisatty-kysymys
-               (select-keys [:kysymysryhmaid :kysymys_fi])))))))
+      (lisaa-kysymys! {:kysymys_fi "Kysymys"} 1)
+      (is (= (:kysymys_fi @lisatty-kysymys) "Kysymys"))
+      (is (= (:kysymysryhmaid @lisatty-kysymys) 1)))))
 
 (deftest lisaa-kysymys-lisaa-monivalintakysymyksen-vaihtoehdon
-  (let [kysymys (-> oletus-kysymys
-                  (assoc :vastaustyyppi "monivalinta")
-                  (assoc :monivalintavaihtoehdot [{:teksti_fi "Vaihtoehto 1"} {:teksti_fi "Vaihtoehto 2"}]))
-        lisatyt-monivalintavaihtoehdot (atom [])
-        kysymysryhmaid 11
-        uusi-kysymysid 12]
-    (with-redefs [aipal.arkisto.kysymysryhma/lisaa-kysymys! (fn [kysymys] (assoc kysymys :kysymysid uusi-kysymysid))
-                  aipal.arkisto.kysymysryhma/lisaa-monivalintavaihtoehto! (partial swap! lisatyt-monivalintavaihtoehdot conj)]
-      (lisaa-kysymys! kysymys kysymysryhmaid)
-      (is (= [{:kysymysid uusi-kysymysid, :teksti_fi "Vaihtoehto 1", :jarjestys 0}
-              {:kysymysid uusi-kysymysid, :teksti_fi "Vaihtoehto 2", :jarjestys 1}]
+  (let [lisatyt-monivalintavaihtoehdot (atom [])]
+    (with-redefs [aipal.arkisto.kysymysryhma/lisaa-kysymys!
+                  (fn [kysymys] (assoc kysymys :kysymysid 2))
+
+                  aipal.arkisto.kysymysryhma/lisaa-monivalintavaihtoehto!
+                  (partial swap! lisatyt-monivalintavaihtoehdot conj)]
+      (lisaa-kysymys! {:vastaustyyppi "monivalinta"
+                       :monivalintavaihtoehdot [{:teksti_fi "Vaihtoehto 1"}
+                                                {:teksti_fi "Vaihtoehto 2"}]}
+                      1)
+      (is (= [{:kysymysid 2, :teksti_fi "Vaihtoehto 1", :jarjestys 0}
+              {:kysymysid 2, :teksti_fi "Vaihtoehto 2", :jarjestys 1}]
              @lisatyt-monivalintavaihtoehdot)))))
