413005042bddb19feb354a6e6ae2f98590abe57d
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-12-09 11:24:52 +0200

    OPH-1102: sisennyksen korjaus

--- aipal/src/clj/aipal/rest_api/vastaajatunnus.clj
+++ aipal/src/clj/aipal/rest_api/vastaajatunnus.clj
@@ -14,9 +14,9 @@
 
 (ns aipal.rest-api.vastaajatunnus
   (:require [compojure.core :as c]
-    [aipal.compojure-util :as cu]
-    [oph.common.util.http-util :refer [json-response parse-iso-date]]
-    [aipal.arkisto.vastaajatunnus :as vastaajatunnus]))
+            [aipal.compojure-util :as cu]
+            [oph.common.util.http-util :refer [json-response parse-iso-date]]
+            [aipal.arkisto.vastaajatunnus :as vastaajatunnus]))
 
 (c/defroutes reitit
   (cu/defapi :vastaajatunnus-luonti kyselykertaid :post "/:kyselykertaid" [kyselykertaid & vastaajatunnus]
