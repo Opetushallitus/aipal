d7044816926a669688ba8244771019c1bb866cc8
Mikko Suonio <mikko.suonio@solita.fi>
2014-12-12 11:37:25 +0200

    OPH-1100: Vertailuraporttien vastaajien maksimimäärä palvelimelta

--- aipal/src/clj/aipal/toimiala/raportti/valtakunnallinen.clj
+++ aipal/src/clj/aipal/toimiala/raportti/valtakunnallinen.clj
@@ -89,6 +89,20 @@
                 :vastaus.vapaateksti)
     sql/exec))
 
+(defn hae-vastaajien-maksimimaara []
+  (->
+    (sql/select* :vastaajatunnus)
+    (sql/join :inner :kyselykerta (= :kyselykerta.kyselykertaid :vastaajatunnus.kyselykertaid))
+    (sql/join :inner :kysely_kysymysryhma (= :kysely_kysymysryhma.kyselyid :kyselykerta.kyselyid))
+    (sql/join :inner :kysymysryhma (= :kysymysryhma.kysymysryhmaid :kysely_kysymysryhma.kysymysryhmaid))
+    (sql/where {:kysymysryhma.valtakunnallinen true})
+    (sql/fields :vastaajatunnus.vastaajatunnusid :vastaajatunnus.vastaajien_lkm)
+    (sql/group :vastaajatunnus.vastaajatunnusid :vastaajatunnus.vastaajien_lkm)
+    sql/exec
+    (->>
+      (map :vastaajien_lkm)
+      (reduce +))))
+
 (defn muodosta [parametrit]
   (let [alkupvm (joda-date->sql-date (parse-iso-date (:vertailujakso_alkupvm parametrit)))
         loppupvm (joda-date->sql-date (parse-iso-date (:vertailujakso_loppupvm parametrit)))
@@ -102,4 +116,5 @@
         vastaukset (hae-vastaukset rajaukset alkupvm loppupvm koulutustoimijat koulutusalatunnus opintoalatunnus tutkintotunnus)]
     {:luontipvm (time/today)
      :raportti  (raportointi/muodosta-raportti-vastauksista kysymysryhmat kysymykset vastaukset)
-     :vastaajien-lkm (count (group-by :vastaajaid vastaukset))}))
+     :vastaajien-lkm (count (group-by :vastaajaid vastaukset))
+     :vastaajien_maksimimaara (hae-vastaajien-maksimimaara)}))
