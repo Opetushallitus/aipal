468b3d691cfec7ad03254918d8f5566370dd537c
Antti Virtanen <antti.virtanen@iki.fi>
2014-12-15 14:54:31 +0200

    OPH-501 suorituskykytestit konfiguroitu paremmin CI-buildeja varten konfiguroitavaksi

--- aipal/test/clj/aipal/toimiala/raportti/kyselykerta_perf.clj
+++ aipal/test/clj/aipal/toimiala/raportti/kyselykerta_perf.clj
@@ -8,23 +8,27 @@
 
 (use-fixtures :each tietokanta-fixture)
                       
-(defn async-http-requ [url user-id context callback]
+(defn async-http-requ [url uid user-id context callback]
   (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))]
-    (http/get url {:headers {"uid" "T-1001"
+    (http/get url {:headers {"uid" uid
                              "x-xsrf-token" "token"
                              "Cookie" "cache=true; XSRF-TOKEN=token"}} check-status)))
 
-(defn kyselekertaid->perf-fn [base-url id]
+(defn kyselekertaid->perf-fn [base-url id userid]
   (let [url (str base-url "/api/raportti/kyselykerta/" id)
-        requ-fn (partial async-http-requ url)
+        requ-fn (partial async-http-requ url userid)
         requ {:name "lol" :fn requ-fn}]
     requ))
   
 (deftest ^:performance kyselykerta-raportti []
   (let [base-url (or (System/getenv "AIPAL_URL") "http://192.168.50.1:8082")
+        userid (or (System/getenv "AIPAL_UID")  "T-1001")
         test-ids (doall (map :kyselykertaid (take 100 (kyselykerta-arkisto/hae-kaikki))))
-        test-reqv (mapv #(kyselekertaid->perf-fn base-url %1) test-ids)]
+        test-reqv (mapv #(kyselekertaid->perf-fn base-url %1 userid) test-ids)]
     (println test-ids)
     (run-simulation
       [{:name "Satunnaistettu kyselykerta raportin suorituskykytesti"
-        :requests test-reqv}] 4)))
+        :requests test-reqv}]
+      4 {:root "target/perf-report"})))
+        
+        
