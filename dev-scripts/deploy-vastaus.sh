005d6a78de3137c90cd1742f3d0484f12a820fbc
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-12-22 14:17:54 +0200

    Aipalvastaukselle deployskripti kehitysvagrantiin

---
+++ dev-scripts/deploy-vastaus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+repo_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+set -x
+
+cd $repo_path/aipal-vastaus
+./build.sh
+
+cd $repo_path/ansible
+chmod 600 yhteiset/dev_id_rsa
+ssh-add yhteiset/dev_id_rsa
+ansible-playbook -v -i aipalvastaus_vagrant/hosts yhteiset/julkaise_paikallinen_versio.yml -e "sovellus_jar=\"$repo_path/aipal-vastaus/target/aipalvastaus-standalone.jar\""
