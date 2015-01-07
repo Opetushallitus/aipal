dd89717a511b6bdec56289d8abfb72e46a6669d6
Jarno Kanto <Jarno.Kanto@solita.fi>
2015-01-07 11:46:00 +0200

    OPH-1188: kuvaustekstit valtakunnallisiin kysymysryhmiin

---
+++ aipal-db/resources/db/migration/V4_1__kuvaukset_valtakunnallisille_ryhmille.sql
@@ -0,0 +1,17 @@
+update kysymysryhma set
+  kuvaus_fi='Hakeutumisvaiheella tarkoitetaan ajankohtaa, jolloin hakeuduit suorittamaan näyttötutkintoa tai sen osaa. Tässä vaiheessa oppilaitos tai oppisopimuskoulutuksen järjestäjä sopii kanssasi henkilökohtaisista järjestelyistä, joiden avulla saat suoritettua tavoitteesi mukaisen tutkinnon tai tutkinnon osan. Järjestelyt kirjataan henkilökohtaiseen suunnitelmaasi.',
+  kuvaus_sv='Med ansökningsskedet avses tidpunkten då du ansökte om att avlägga examen eller del av examen. I detta skede avtalar läroanstalten eller anordnaren av läroavtalsutbildningen med dig om de personliga arrangemang med hjälp av vilka du kan avlägga den examen eller del av examen som är ditt mål. Arrangemangen antecknas i din personliga plan.'
+where kysymysryhmaid=3341886
+  and valtakunnallinen=true;
+
+update kysymysryhma set
+  kuvaus_fi='Näyttötutkinnoissa ammattitaito osoitetaan ja arvioidaan tutkintotilaisuuksissa. Tutkinnon suorittamisella tarkoitetaan tilanteita, joissa osoitit tutkinnon perusteissa vaaditun osaamisen ja osaamisesi arvioitiin.',
+  kuvaus_sv='I fristående examina visas och bedöms yrkesskickligheten vid examenstillfällen. Med avläggandet av examen avses de situationer där du visade det kunnande som krävs i examensgrunderna och där ditt kunnande bedömdes.'
+where kysymysryhmaid=3341887
+  and valtakunnallinen=true;
+
+update kysymysryhma set
+  kuvaus_fi='Tarvittavan ammattitaidon hankkimisella tarkoitetaan oppimista, jota tarvitset tutkintosi suorittamiseksi. Oppimista tapahtuu työpaikalla, oppilaitoksessa ja esimerkiksi verkossa.',
+  kuvaus_sv='Med förvärvandet av behövlig yrkesskicklighet avses det lärande du behöver för att avlägga din examen. Lärandet sker på en arbetsplats, i en läroanstalt och till exempel på webben.'
+where kysymysryhmaid=3341888
+  and valtakunnallinen=true;
