e43a65ee2daeb130e6cd483b6cb74c9836521744
Timo Mihaljov <timo.mihaljov@solita.fi>
2015-01-21 15:41:42 +0200

    OPH-1244: Eriytetään KyselylistaController

--- aipal/frontend/src/js/yhteiset/direktiivit/kyselylista.js
+++ aipal/frontend/src/js/yhteiset/direktiivit/kyselylista.js
@@ -16,6 +16,79 @@
 
 angular.module('yhteiset.direktiivit.kyselylista', ['yhteiset.palvelut.i18n', 'yhteiset.palvelut.ilmoitus'])
 
+  .controller('KyselylistaController', ['$filter', '$scope', '$location', 'Kysely', 'Kyselykerta', 'ilmoitus', 'i18n', 'varmistus', function($filter, $scope, $location, Kysely, Kyselykerta, ilmoitus, i18n, varmistus) {
+    $scope.julkaiseKyselyModal = function(kysely) {
+      varmistus.varmista(i18n.hae('kysely.julkaise'), $filter('lokalisoiKentta')(kysely, 'nimi'), i18n.hae('kysely.julkaise_ohjeistus'), i18n.hae('kysely.julkaise')).then(function() {
+        Kysely.julkaise(kysely.kyselyid)
+          .success(function(response) {
+            _.assign(kysely, response);
+            ilmoitus.onnistuminen(i18n.hae('kysely.julkaisu_onnistui'));
+          })
+          .error(function() {
+            ilmoitus.virhe(i18n.hae('kysely.julkaisu_epaonnistui'));
+          });
+      });
+    };
+
+    $scope.poistaKyselyModal = function(kyselylista, kysely) {
+      varmistus.varmista(i18n.hae('kysely.poista'), $filter('lokalisoiKentta')(kysely, 'nimi'), i18n.hae('kysely.poista_ohjeistus'), i18n.hae('kysely.poista')).then(function() {
+        var kyselyid = kysely.kyselyid;
+        var kyselyindex = _.findIndex(kyselylista, {kyselyid: kyselyid});
+        Kysely.poista(kyselyid)
+          .success(function() {
+            kyselylista.splice(kyselyindex, 1);
+            ilmoitus.onnistuminen(i18n.hae('kysely.poisto_onnistui'));
+          })
+          .error(function() {
+            ilmoitus.virhe(i18n.hae('kysely.poisto_epaonnistui'));
+          });
+      });
+    };
+
+    $scope.uusiKyselykerta = function (kysely) {
+      $location.url('/kyselyt/' + kysely.kyselyid + '/kyselykerta/uusi');
+    };
+
+    $scope.suljeKyselyModal = function(kysely) {
+      varmistus.varmista(i18n.hae('kysely.sulje'), $filter('lokalisoiKentta')(kysely, 'nimi'), i18n.hae('kysely.sulje_ohjeistus'), i18n.hae('kysely.sulje')).then(function() {
+        Kysely.sulje(kysely.kyselyid).success(function(uusiKysely) {
+          _.assign(kysely, uusiKysely);
+          ilmoitus.onnistuminen(i18n.hae('kysely.sulkeminen_onnistui'));
+        });
+      });
+    };
+
+    $scope.palautaKysely = function(kysely) {
+      Kysely.palauta(kysely.kyselyid).success(function(uusiKysely) {
+        _.assign(kysely, uusiKysely);
+        ilmoitus.onnistuminen(i18n.hae('kysely.palautus_onnistui'));
+      });
+    };
+    $scope.palautaLuonnokseksi = function(kysely) {
+      Kysely.palautaLuonnokseksi(kysely.kyselyid).success(function(uusiKysely) {
+        _.assign(kysely, uusiKysely);
+        ilmoitus.onnistuminen(i18n.hae('kysely.palautus_onnistui'));
+      });
+    };
+    $scope.lukitseKyselykerta = function(kyselykerta) {
+      varmistus.varmista(i18n.hae('kyselykerta.lukitse'), kyselykerta.nimi, i18n.hae('kyselykerta.lukitse_teksti'), i18n.hae('kyselykerta.lukitse')).then(function() {
+        Kyselykerta.lukitse(kyselykerta.kyselykertaid)
+          .success(function(uusiKyselykerta) {
+            _.assign(kyselykerta, uusiKyselykerta);
+            ilmoitus.onnistuminen(i18n.hae('kyselykerta.lukitseminen_onnistui'));
+          });
+      });
+    };
+    $scope.avaaKyselykerta = function(kyselykerta) {
+      Kyselykerta.avaa(kyselykerta.kyselykertaid)
+        .success(function(uusiKyselykerta) {
+          _.assign(kyselykerta, uusiKyselykerta);
+        });
+    };
+
+    $scope.kyselykertojen_lajitteluperuste = 'luotuaika';
+  }])
+
   .directive('kyselylista', [function() {
     return {
       restrict: 'E',
@@ -26,78 +99,7 @@ angular.module('yhteiset.direktiivit.kyselylista', ['yhteiset.palvelut.i18n', 'y
         haku: '='
       },
       templateUrl : 'template/yhteiset/direktiivit/kyselylista.html',
-      controller: ['$filter', '$scope', '$location', 'Kysely', 'Kyselykerta', 'ilmoitus', 'i18n', 'varmistus', function($filter, $scope, $location, Kysely, Kyselykerta, ilmoitus, i18n, varmistus) {
-        $scope.julkaiseKyselyModal = function(kysely) {
-          varmistus.varmista(i18n.hae('kysely.julkaise'), $filter('lokalisoiKentta')(kysely, 'nimi'), i18n.hae('kysely.julkaise_ohjeistus'), i18n.hae('kysely.julkaise')).then(function() {
-            Kysely.julkaise(kysely.kyselyid)
-              .success(function(response) {
-                _.assign(kysely, response);
-                ilmoitus.onnistuminen(i18n.hae('kysely.julkaisu_onnistui'));
-              })
-              .error(function() {
-                ilmoitus.virhe(i18n.hae('kysely.julkaisu_epaonnistui'));
-              });
-          });
-        };
-
-        $scope.poistaKyselyModal = function(kyselylista, kysely) {
-          varmistus.varmista(i18n.hae('kysely.poista'), $filter('lokalisoiKentta')(kysely, 'nimi'), i18n.hae('kysely.poista_ohjeistus'), i18n.hae('kysely.poista')).then(function() {
-            var kyselyid = kysely.kyselyid;
-            var kyselyindex = _.findIndex(kyselylista, {kyselyid: kyselyid});
-            Kysely.poista(kyselyid)
-              .success(function() {
-                kyselylista.splice(kyselyindex, 1);
-                ilmoitus.onnistuminen(i18n.hae('kysely.poisto_onnistui'));
-              })
-              .error(function() {
-                ilmoitus.virhe(i18n.hae('kysely.poisto_epaonnistui'));
-              });
-          });
-        };
-
-        $scope.uusiKyselykerta = function (kysely) {
-          $location.url('/kyselyt/' + kysely.kyselyid + '/kyselykerta/uusi');
-        };
-
-        $scope.suljeKyselyModal = function(kysely) {
-          varmistus.varmista(i18n.hae('kysely.sulje'), $filter('lokalisoiKentta')(kysely, 'nimi'), i18n.hae('kysely.sulje_ohjeistus'), i18n.hae('kysely.sulje')).then(function() {
-            Kysely.sulje(kysely.kyselyid).success(function(uusiKysely) {
-              _.assign(kysely, uusiKysely);
-              ilmoitus.onnistuminen(i18n.hae('kysely.sulkeminen_onnistui'));
-            });
-          });
-        };
-
-        $scope.palautaKysely = function(kysely) {
-          Kysely.palauta(kysely.kyselyid).success(function(uusiKysely) {
-            _.assign(kysely, uusiKysely);
-            ilmoitus.onnistuminen(i18n.hae('kysely.palautus_onnistui'));
-          });
-        };
-        $scope.palautaLuonnokseksi = function(kysely) {
-          Kysely.palautaLuonnokseksi(kysely.kyselyid).success(function(uusiKysely) {
-            _.assign(kysely, uusiKysely);
-            ilmoitus.onnistuminen(i18n.hae('kysely.palautus_onnistui'));
-          });
-        };
-        $scope.lukitseKyselykerta = function(kyselykerta) {
-          varmistus.varmista(i18n.hae('kyselykerta.lukitse'), kyselykerta.nimi, i18n.hae('kyselykerta.lukitse_teksti'), i18n.hae('kyselykerta.lukitse')).then(function() {
-            Kyselykerta.lukitse(kyselykerta.kyselykertaid)
-              .success(function(uusiKyselykerta) {
-                _.assign(kyselykerta, uusiKyselykerta);
-                ilmoitus.onnistuminen(i18n.hae('kyselykerta.lukitseminen_onnistui'));
-              });
-          });
-        };
-        $scope.avaaKyselykerta = function(kyselykerta) {
-          Kyselykerta.avaa(kyselykerta.kyselykertaid)
-            .success(function(uusiKyselykerta) {
-              _.assign(kyselykerta, uusiKyselykerta);
-            });
-        };
-
-        $scope.kyselykertojen_lajitteluperuste = 'luotuaika';
-      }]
+      controller: 'KyselylistaController'
     };
   }])
 ;
