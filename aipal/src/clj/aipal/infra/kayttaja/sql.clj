7a23b0c49f92be10de1560d996a0164023186191
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-01-26 14:42:43 +0200

    Logitetaan with-sql-kayttaja-makrossa tapahtuvat virheet

--- aipal/src/clj/aipal/infra/kayttaja/sql.clj
+++ aipal/src/clj/aipal/infra/kayttaja/sql.clj
@@ -1,12 +1,16 @@
 (ns aipal.infra.kayttaja.sql
   (:require [korma.core :as sql]
-            [korma.db :as db]))
+            [korma.db :as db]
+            [clojure.tools.logging :as log]))
 
 (defn with-sql-kayttaja* [oid f]
   (db/transaction
     (try
       (sql/exec-raw (str "set aipal.kayttaja = '" oid "';"))
       (f)
+      (catch Throwable t
+        (log/error "Virhe tietokantafunktiossa" t)
+        (throw t))
       (finally
         (sql/exec-raw (str "reset aipal.kayttaja;"))))))
 
