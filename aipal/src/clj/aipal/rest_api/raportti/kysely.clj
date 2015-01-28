ae48ff73681ceeaed740c0644b2505ad9556ac85
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-01-28 15:02:31 +0200

    OPH-1270: Valtakunnallinen vertailuarvo kyselyraportille.

--- aipal/src/clj/aipal/rest_api/raportti/kysely.clj
+++ aipal/src/clj/aipal/rest_api/raportti/kysely.clj
@@ -18,8 +18,9 @@
             [oph.common.util.http-util :refer [json-response parse-iso-date csv-download-response]]
             [oph.common.util.util :refer [paivita-arvot poista-tyhjat]]
             [oph.korma.korma :refer [joda-date->sql-date]]
-            [aipal.toimiala.raportti.kysely :refer [muodosta-raportti]]
-            [aipal.toimiala.raportti.raportointi :refer [muodosta-csv muodosta-tyhja-csv]]))
+            [aipal.toimiala.raportti.kysely :refer [muodosta-raportti muodosta-valtakunnallinen-vertailuraportti]]
+            [aipal.toimiala.raportti.raportointi :refer [muodosta-csv muodosta-tyhja-csv]]
+            [oph.common.util.http-util :refer [csv-download-response]]))
 
 (defn muodosta-raportti-parametreilla
   [kyselyid parametrit]
@@ -33,11 +34,13 @@
   (cu/defapi :kysely-raportti kyselyid :post "/:kyselyid" [kyselyid & parametrit]
     (db/transaction
       (let [vaaditut-vastaajat (:raportointi-minimivastaajat asetukset)
+            valtakunnallinen-raportti (muodosta-valtakunnallinen-vertailuraportti (Integer/parseInt kyselyid) parametrit)
             raportti (muodosta-raportti-parametreilla kyselyid parametrit)]
         (json-response
-          [(if (>= (:vastaajien-lkm raportti) vaaditut-vastaajat)
-            raportti
-            (assoc (dissoc raportti :raportti) :virhe "ei-riittavasti-vastaajia"))])))))
+          (for [raportti [valtakunnallinen-raportti raportti]]
+            (if (>= (:vastaajien-lkm raportti) vaaditut-vastaajat)
+              raportti
+              (assoc (dissoc raportti :raportti) :virhe "ei-riittavasti-vastaajia"))))))))
 
 (defn csv-reitit [asetukset]
   (cu/defapi :kysely-raportti kyselyid :get "/:kyselyid/:kieli/csv" [kyselyid kieli & parametrit]
--- aipal/src/clj/aipal/toimiala/raportti/kysely.clj
+++ aipal/src/clj/aipal/toimiala/raportti/kysely.clj
@@ -18,7 +18,9 @@
             [aipal.integraatio.sql.korma :refer [kyselykerta]]
             [oph.korma.korma :refer [select-unique-or-nil]]
             [aipal.toimiala.raportti.kyselyraportointi :as kyselyraportointi]
-            [aipal.toimiala.raportti.raportointi :as raportointi]))
+            [aipal.toimiala.raportti.raportointi :as raportointi]
+            [aipal.arkisto.kysely :as arkisto]
+            [aipal.toimiala.raportti.valtakunnallinen :as valtakunnallinen-raportti]))
 
 (defn ^:private hae-kysely [kyselyid]
   (select-unique-or-nil :kysely
@@ -29,6 +31,16 @@
     (sql/fields :kysely.kyselyid [:kysely.nimi_fi :kysely_fi] [:kysely.nimi_sv :kysely_sv] :kysely.voimassa_alkupvm :kysely.voimassa_loppupvm
                 [:koulutustoimija.nimi_fi :koulutustoimija_fi] [:koulutustoimija.nimi_sv :koulutustoimija_sv])
     (sql/where {:kyselyid kyselyid})))
+
+(defn muodosta-valtakunnallinen-vertailuraportti [kyselyid parametrit]
+  (let [taustakysymysryhmaid (arkisto/hae-kyselyn-taustakysymysryhmaid kyselyid)
+        kysymysidt (arkisto/hae-kyselyn-valtakunnallisten-kysymysryhmien-kysymysidt kyselyid)
+        parametrit (assoc parametrit :taustakysymysryhmaid (str taustakysymysryhmaid)
+                                     :tyyppi "vertailu"
+                                     :tutkintorakennetaso "tutkinto"
+                                     :kysymykset (into {} (for [kysymysid kysymysidt]
+                                                            {kysymysid {:monivalinnat {}}})))]
+    (valtakunnallinen-raportti/muodosta parametrit)))
 
 (defn muodosta-raportti [kyselyid parametrit]
   (when-let [kysely (hae-kysely kyselyid)]
