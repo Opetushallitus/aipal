223c07b12cc4b1c46bc90cd5ce48856f6eb821a1
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-01-29 11:12:29 +0200

    OPH-1274: Ääkkösten konversio properties-muotoon.

--- dev-scripts/kielet-csv-konversio.sh
+++ dev-scripts/kielet-csv-konversio.sh
@@ -7,5 +7,11 @@ grep -v '^;;$'              | # tyhjät rivit pois
 gsed 's/"\("\?\)/\1/g'      | # lainausmerkit pois, paitsi "" -> "
 gsed 's/;$//g'              | # ; rivin lopussa pois
 gsed 's/ *$//g'             | # välilyönnit rivin lopussa pois
-gsed 's/;.*$//g'              # ylimääräiset sarakkeet (kommentit) pois
+gsed 's/;.*$//g'            | # ylimääräiset sarakkeet (kommentit) pois
+gsed 's/å/\\u00e5/g'        | # umlautit properties-muotoon
+gsed 's/ä/\\u00e4/g'        |
+gsed 's/ö/\\u00f6/g'        |
+gsed 's/Å/\\u00c5/g'        |
+gsed 's/Ä/\\u00c4/g'        |
+gsed 's/Ö/\\u00d6/g'
 
