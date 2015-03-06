cbf2d5afc984a943f6d33669be9b85b6c838bc7c
Mikko Suonio <mikko.suonio@solita.fi>
2015-03-06 14:12:02 +0200

    OPH-1302: Näytä kyselykerran nimi virheilmoituksessa

--- aipal/src/clj/aipal/toimiala/raportti/kyselykerta.clj
+++ aipal/src/clj/aipal/toimiala/raportti/kyselykerta.clj
@@ -45,4 +45,5 @@
        :luontipvm (time/today)
        :vastaajien_maksimimaara (kyselyraportointi/hae-vastaajien-maksimimaara parametrit)
        :vastaajien_lukumaara (kyselyraportointi/laske-vastaajat-yhteensa koulutustoimijatiedot)
-       :raportti (map raportointi/laske-kysymysryhman-vastaajat (kyselyraportointi/muodosta-raportti parametrit))})))
+       :raportti (map raportointi/laske-kysymysryhman-vastaajat (kyselyraportointi/muodosta-raportti parametrit))
+       :nimi (:kyselykerta kyselykerta)})))
