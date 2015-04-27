538f00510aac4aa99d6618c8bf413f94dc388c94
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-04-27 13:40:40 +0300

    DateTimen json-muunto

--- aipal/src/clj/aipal/palvelin.clj
+++ aipal/src/clj/aipal/palvelin.clj
@@ -122,11 +122,14 @@
   "Ring-wrapperit ja compojure-reitit ilman HTTP-palvelinta"
   [asetukset]
   (require 'aipal.reitit)
-  (let [session-store (memory-store)
-        reitit ((eval 'aipal.reitit/reitit) asetukset)
-          _ (json-gen/add-encoder org.joda.time.LocalDate
+  (json-gen/add-encoder org.joda.time.LocalDate
               (fn [c json-generator]
-                (.writeString json-generator (.toString c "yyyy-MM-dd"))))]
+                (.writeString json-generator (.toString c "yyyy-MM-dd"))))
+  (json-gen/add-encoder org.joda.time.DateTime
+      (fn [c json-generator]
+        (.writeString json-generator (.toString c))))
+  (let [session-store (memory-store)
+        reitit ((eval 'aipal.reitit/reitit) asetukset)]
     (-> reitit
       wrap-internal-forbidden
       wrap-keyword-params
