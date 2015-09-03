d337659c29c8a5c3c05fbba73fda8f33cd38647b
Mikko Suonio <mikko.suonio@solita.fi>
2015-09-03 15:57:50 +0300

    OPH-1434: Korjaa suorituskykytestit
    Toteutus vaatii nyt kyselykertojen hakua koulutustoimijakohtaisesti.
    Lisäsin testien parametreihin käytettävän koulutustoimijan.

--- aipal/test/clj/aipal/toimiala/raportti/perftest_util.clj
+++ aipal/test/clj/aipal/toimiala/raportti/perftest_util.clj
@@ -1,5 +1,5 @@
 (ns aipal.toimiala.raportti.perftest-util
-  (:require 
+  (:require
     [org.httpkit.client :as http]
     [cheshire.core :refer :all]))
 
@@ -14,14 +14,14 @@
   {:base-url (or (System/getenv "AIPAL_URL") "http://192.168.50.1:8082")
    :userid (or (System/getenv "AIPAL_UID")  "T-1001")
    :basic-auth (or (System/getenv "AIPAL_AUTH") "pfft:thx")
-   :request-count (cond 
+   :request-count (cond
                     (nil? (System/getenv "AIPAL_URL")) 1
-                    :else 50)}
-  )
+                    :else 50)
+   :kyselykerta-perf {:koulutustoimija "0213834-5"}})
 
 (defn async-http-requ [url uid basic-auth user-id context callback]
   (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))]
-    (http/get url (auth-params uid basic-auth) 
+    (http/get url (auth-params uid basic-auth)
       check-status)))
 
 (defn async-http-json-requ [url uid basic-auth body user-id context callback]
