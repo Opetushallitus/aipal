21d197934a1ea0533135caf30b2947c573747169
Antti Virtanen <antti.virtanen@iki.fi>
2015-11-05 13:02:24 +0200

    AVOP-32 Ruotsinkielisen virheilmoituksen tekstissä AIPAL -> AVOP.

--- aipal/src/clj/avop/infra/kayttaja/middleware.clj
+++ aipal/src/clj/avop/infra/kayttaja/middleware.clj
@@ -7,5 +7,5 @@
           "AVOP-palautejärjestelmän käyttö edellyttää käyttöoikeuksia, jotka sinun käyttäjätunnukseltasi puuttuvat. "
           "Tarvittaessa ota yhteyttä oman organisaatiosi AVOP-koulutustoimijan pääkäyttäjään tai OKM:n AVOP-neuvontaan (avop@postit.csc.fi).\n\n"
           "Användningen av responssystemet AVOP förutsätter användarrättigheter. I din användarkod finns ej användarrättigheter. "
-          "Kontakta vid behov huvudanvändaren för AVOP i din egen organisation eller UBS AIPAL-rådgivning (avop@postit.csc.fi). ")]
+          "Kontakta vid behov huvudanvändaren för AVOP i din egen organisation eller UBS AVOP-rådgivning (avop@postit.csc.fi). ")]
     (wrap-kayttaja handler paasykielletty-virheilmoitus)))
