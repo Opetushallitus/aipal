e970fa79fcd6034e5fda9a4948fe6928355bfd47
Heikki Hokkanen <hoxu@users.sf.net>
2015-12-04 13:47:55 +0200

    Järjestä project.clj riippuvuudet

--- aipal/project.clj
+++ aipal/project.clj
@@ -14,46 +14,35 @@
 
 (defproject aipal "0.1.0-SNAPSHOT"
   :description "AIPAL"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.cache "0.6.4"]
-                 [ring/ring-core "1.2.1"]
-                 [http-kit "2.1.18"]
-                 [clj-http "1.0.1"]
-
-                 [compojure "1.3.3"]
-                 [ring/ring-json "0.2.0"]
-                 [ring/ring-headers "0.1.0"]
-                 [ring/ring-session-timeout "0.1.0"]
+  :dependencies [[cas-single-sign-out "0.1.2" :exclusions [clj-cas-client]]
+                 [ch.qos.logback/logback-classic "1.0.13"]
                  [cheshire "5.4.0"]
+                 [clj-http "1.0.1"]
+                 [clj-time "0.9.0"]
+                 [clojure-csv/clojure-csv "2.0.1"]
+                 [clojurewerkz/quartzite "1.3.0"]
+                 [com.cemerick/valip "0.3.2"]
+                 [com.jolbox/bonecp "0.8.0.RELEASE"]
+                 [compojure "1.3.3"]
+                 [http-kit "2.1.18"]
+                 [korma "0.4.0"]
                  [metosin/compojure-api "0.8.2"]
                  [metosin/ring-swagger-ui "2.0.10-1"]
-
-                 [org.clojars.noidi/clj-cas-client
-                  "0.0.6-4ae43963cb458579a3813f9dda4fba52ad4d9607-ring-1.2.1"
-                  :exclusions [ring]]
-                 [cas-single-sign-out "0.1.2"
-                  :exclusions [clj-cas-client]]
-
-                 [org.clojure/tools.logging "0.2.6"]
-                 [ch.qos.logback/logback-classic "1.0.13"]
-                 [org.slf4j/slf4j-api "1.7.5"]
-                 [clj-time "0.9.0"]
-                 [com.cemerick/valip "0.3.2"]
-                 [prismatic/schema "0.4.0"]
-
-                 [korma "0.4.0"]
-                 [org.postgresql/postgresql "9.3-1101-jdbc41"]
-                 [com.jolbox/bonecp "0.8.0.RELEASE"]
-
-                 [clojurewerkz/quartzite "1.3.0"]
+                 [org.clojars.noidi/clj-cas-client "0.0.6-4ae43963cb458579a3813f9dda4fba52ad4d9607-ring-1.2.1" :exclusions [ring]]
                  [org.clojars.pntblnk/clj-ldap "0.0.9"]
-
-                 [robert/hooke "1.3.0"]
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.cache "0.6.4"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.postgresql/postgresql "9.3-1101-jdbc41"]
+                 [org.slf4j/slf4j-api "1.7.5"]
                  [peridot "0.3.0"]
-
-                 [stencil "0.3.2"]
-
-                 [clojure-csv/clojure-csv "2.0.1"]]
+                 [prismatic/schema "0.4.0"]
+                 [ring/ring-core "1.2.1"]
+                 [ring/ring-headers "0.1.0"]
+                 [ring/ring-json "0.2.0"]
+                 [ring/ring-session-timeout "0.1.0"]
+                 [robert/hooke "1.3.0"]
+                 [stencil "0.3.2"]]
   :plugins [[test2junit "1.0.1"]
             [codox "0.8.12"]]
   :profiles {:dev {:source-paths ["dev"]
