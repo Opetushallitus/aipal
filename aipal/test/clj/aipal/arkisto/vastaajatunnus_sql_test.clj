6e812c2100d5116c3abc24cde4c54c9acd6f482d
Heikki Hokkanen <hoxu@users.sf.net>
2015-12-04 09:05:24 +0200

    Poista käyttämättömiä requireja

--- aipal/test/clj/aipal/arkisto/vastaajatunnus_sql_test.clj
+++ aipal/test/clj/aipal/arkisto/vastaajatunnus_sql_test.clj
@@ -1,10 +1,8 @@
 (ns aipal.arkisto.vastaajatunnus-sql-test
   (:require [clojure.test :refer :all]
-            [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clj-time.core :as time]
             clj-time.coerce
             [korma.core :as sql]
             [aipal.integraatio.sql.korma :as taulut]
