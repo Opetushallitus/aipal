796f69a10c2b757051033513bfa6eaf7494d3034
Heikki Hokkanen <hoxu@users.sf.net>
2015-12-04 11:10:46 +0200

    OPH-1037: Testi vastaajatunnus.muokattavissa-tiedolle

--- aipal/test/clj/aipal/arkisto/vastaajatunnus_sql_test.clj
+++ aipal/test/clj/aipal/arkisto/vastaajatunnus_sql_test.clj
@@ -3,7 +3,8 @@
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
-            clj-time.coerce
+            [clj-time.core :as ctime]
+            [clj-time.coerce :as c]
             [korma.core :as sql]
             [aipal.integraatio.sql.korma :as taulut]
             [aipal.sql.test-util :refer :all]
@@ -70,6 +71,24 @@
                :vastaajien_lkm 6})
       (is (= (count (hae-kyselykerralla k))
              6)))))
+
+(deftest ^:integraatio muokattavissa-tieto-lasketaan-oikein
+  (testing "vastaajatunnus.voimassa_loppupvm tänään"
+    (let [vastaajatunnus (first (lisaa-vastaajatunnus! {:voimassa_loppupvm (c/to-date-time (ctime/now))}))]
+      (is (:muokattavissa vastaajatunnus))))
+  (testing "vastaajatunnus.voimassa_loppupvm liian vanha"
+    (let [vastaajatunnus (first (lisaa-vastaajatunnus! {:voimassa_loppupvm (c/to-date-time (ctime/date-time 2015 1 1))}))]
+      (is (not (:muokattavissa vastaajatunnus)))))
+  (testing "kysely.voimassa_loppupvm tänään, muut null"
+    (let [kysely (lisaa-kysely! {:voimassa_loppupvm (c/to-date-time (ctime/now))})
+          kyselykerta (lisaa-kyselykerta! {:voimassa_loppupvm nil} kysely)
+          vastaajatunnus (first (lisaa-vastaajatunnus! {:voimassa_loppupvm nil} kyselykerta))]
+      (is (:muokattavissa vastaajatunnus))))
+  (testing "kysely/kyselykerta/vastaajatunnus voimassa_loppupvm null kaikissa"
+    (let [kysely (lisaa-kysely! {:voimassa_loppupvm nil})
+          kyselykerta (lisaa-kyselykerta! {:voimassa_loppupvm nil} kysely)
+          vastaajatunnus (first (lisaa-vastaajatunnus! {:voimassa_loppupvm nil} kyselykerta))]
+      (is (:muokattavissa vastaajatunnus)))))
 
 (defn vastaajatunnus-gen [kyselykertaid]
   (gen/hash-map :kyselykertaid (gen/return kyselykertaid)
