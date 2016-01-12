88c6d5e46560f7b99efe64a7d206a43813623dfb
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2016-01-12 10:06:06 +0200

    Päivitä opetushallitus-aitu-e2e 0.21.0 -> 0.25.0
    Muuten testejä ei voi ajaa Firefoxin 43.0.4 -versiolla.

--- e2e/project.clj
+++ e2e/project.clj
@@ -16,7 +16,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [korma "0.3.2"]
                  [org.postgresql/postgresql "9.3-1101-jdbc41"]
-                 [solita/opetushallitus-aitu-e2e "0.21.0"]
+                 [solita/opetushallitus-aitu-e2e "0.25.0"]
                  [clj-time "0.6.0"]
                  [com.jolbox/bonecp "0.8.0.RELEASE"]]
   :plugins [[test2junit "1.0.1"]]
