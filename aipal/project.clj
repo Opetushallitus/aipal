ca9a1113101b0df3aabf9dcb190845af43b665be
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2016-02-02 10:31:30 +0200

    OPH-1700: Käytä submodule-riippuvuutta ulkoisen sijaan

--- aipal/project.clj
+++ aipal/project.clj
@@ -53,7 +53,7 @@
              :uberjar {:main aipal.palvelin
                        :aot :all}
              :test {:resource-paths ["test-resources"]}}
-  :source-paths ["src/clj" "../../clojure-utils/src/clj"]
+  :source-paths ["src/clj" "clojure-utils/src/clj"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7"]
   :test-paths ["test/clj"]
