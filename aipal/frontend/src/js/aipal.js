81068ccd6f9558da59043095fe91c289ab49b333
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2016-02-05 09:45:10 +0200

    OPH-1707: Korjaa $cookies -kutsu
    API muuttui angular 1.3 -> 1.4 -päivityksessä.

--- aipal/frontend/src/js/aipal.js
+++ aipal/frontend/src/js/aipal.js
@@ -236,7 +236,7 @@ angular.module('aipal', [
   }])
 
   .run(['$cookies', function($cookies) {
-    $.fn.select2.ajaxDefaults.params.headers = {'x-xsrf-token' : $cookies['XSRF-TOKEN']};
+    $.fn.select2.ajaxDefaults.params.headers = {'x-xsrf-token' : $cookies.get('XSRF-TOKEN')};
   }])
 
   .directive('kielenVaihto', ['kieli', function (kieli) {
