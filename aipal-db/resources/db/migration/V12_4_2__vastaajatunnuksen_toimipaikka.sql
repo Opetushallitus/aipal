f3ed96fb7f6984c24123d7daa98f96d4cc54b21f
Oscar Perez <oscar.perez@gofore.com>
2016-03-24 15:32:43 +0200

    Added quick-and-dirty check to avoid database cleaning

--- aipal-db/resources/db/migration/V12_4_2__vastaajatunnuksen_toimipaikka.sql
+++ aipal-db/resources/db/migration/V12_4_2__vastaajatunnuksen_toimipaikka.sql
@@ -1,2 +1,13 @@
-ALTER TABLE vastaajatunnus ADD COLUMN valmistavan_koulutuksen_toimipaikka VARCHAR(10)
-  REFERENCES toimipaikka(toimipaikkakoodi);
+DO
+$$
+BEGIN
+  IF NOT EXISTS (SELECT column_name
+    FROM information_schema.columns
+    WHERE table_schema='public' and table_name='vastaajatunnus' and column_name='valmistavan_koulutuksen_toimipaikka')
+  THEN
+     ALTER TABLE vastaajatunnus ADD COLUMN valmistavan_koulutuksen_toimipaikka VARCHAR(10) REFERENCES toimipaikka(toimipaikkakoodi);
+  ELSE
+    raise NOTICE 'Already exists';
+  END IF;
+END
+$$
