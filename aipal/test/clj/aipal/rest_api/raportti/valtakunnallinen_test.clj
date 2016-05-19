79c58333b8a27dc6094a8429281e71b381cbbf09
Antti Virtanen <antti.virtanen@iki.fi>
2016-05-19 14:44:41 +0300

    OPH-1745 Refaktoroitu testikoodia rakenteellisesti paremmaksi.

--- aipal/test/clj/aipal/rest_api/raportti/valtakunnallinen_test.clj
+++ aipal/test/clj/aipal/rest_api/raportti/valtakunnallinen_test.clj
@@ -24,35 +24,42 @@
        "{\"monivalinnat\":{}},\"7312029\":{\"monivalinnat\":{}},\"7312030\":{\"monivalinnat\":{}},\"7312031\":{\"monivalinnat\":{}},\"7312032\":"
        "{\"monivalinnat\":{}},\"7312033\":{\"monivalinnat\":{}},\"7312039\":{\"monivalinnat\":{}}}}"))
        
+(def kehitysraportti-ketjutettu-json
+  (str "{\"kieli\":\"fi\",\"tyyppi\":\"kehitys-ketjutettu\",\"tutkintorakennetaso\":\"tutkinto\",\"koulutusalat\":[],\"opintoalat\":[],"
+       "\"tutkinnot\":[\"X00002\",\"X00001\"],"
+       "\"koulutuksen_jarjestajat\":[],\"jarjestavat_oppilaitokset\":[],\"koulutustoimijat\":[],"
+       "\"oppilaitokset\":[],\"taustakysymysryhmaid\":\"3341885\",\"kysymykset\":{\"7312027\":{\"monivalinnat\":{}},\"7312028\":"
+       "{\"monivalinnat\":{}},\"7312029\":{\"monivalinnat\":{}},\"7312030\":{\"monivalinnat\":{}},\"7312031\":{\"monivalinnat\":{}},\"7312032\":"
+       "{\"monivalinnat\":{}},\"7312033\":{\"monivalinnat\":{}},\"7312039\":{\"monivalinnat\":{}}}}"))
+       
 (defn poista-luontipvm-kentat [raportti]
   (clojure.walk/postwalk #(if (map? %) (dissoc % :luontipvm) %) raportti))
-  
+
+(defn tarkista-valtakunnallinen-raportti
+  [input-json output-file]
+  (let [response (-> (session)
+                   (peridot/request "/api/raportti/valtakunnallinen"
+                                    :request-method :post
+                                    :body input-json)
+                   :response)
+        vastaus (body-json response)]
+    (is (= (:status response) 200))
+    (let [oikea-raportti (clojure.edn/read-string (slurp output-file))]
+      ; (spit "filetto" (with-out-str (clojure.pprint/pprint vastaus)))
+      (is (= (poista-luontipvm-kentat oikea-raportti) (poista-luontipvm-kentat vastaus))))))
+ 
+
 (deftest ^:integraatio muodosta-vertailuraportti
   (testing "vertailuraportin perustapaus"
-    (let [response (-> (session)
-                     (peridot/request "/api/raportti/valtakunnallinen"
-                                      :request-method :post
-                                      :body perustapaus-json)
-                     :response)
-          vastaus (body-json response)]
-      (is (= (:status response) 200))
-      (let [oikea-raportti (clojure.edn/read-string (slurp "test/resources/vertailuraportin-perustapaus.edn"))]
-        (is (= (poista-luontipvm-kentat oikea-raportti) (poista-luontipvm-kentat vastaus)))        
-        ; (spit "filetto" (with-out-str (clojure.pprint/pprint vastaus)))
-        ))))
+    (tarkista-valtakunnallinen-raportti perustapaus-json "test/resources/vertailuraportin-perustapaus.edn")))
       
 (deftest ^:integraatio muodosta-kehitysraportti
   (testing "Kehitysraportin perustapaus"
-    (let [response (-> (session)
-                     (peridot/request "/api/raportti/valtakunnallinen"
-                                      :request-method :post
-                                      :body kehitysraportti-json)
-                     :response)
-          vastaus (body-json response)]
-      (is (= (:status response) 200))
-      (let [oikea-raportti (clojure.edn/read-string (slurp "test/resources/kehitysraportin-perustapaus.edn"))]
-        (is (= (poista-luontipvm-kentat oikea-raportti) (poista-luontipvm-kentat vastaus)))        
-        ))))
+    (tarkista-valtakunnallinen-raportti kehitysraportti-json "test/resources/kehitysraportin-perustapaus.edn")))
+
+;(deftest ^:integraatio muodosta-ketjutettu-kehitysraportti
+;  (testing "Kehitysraportin perustapaus"
+;    (tarkista-valtakunnallinen-raportti kehitysraportti-ketjutettu-json "test/resources/kehitysraportin-perustapaus.edn")))
 
 (deftest ^:integraatio muodosta-tutkintovertailun-parametrit-test
   (are [opintoalat koulutusalat odotettu-tulos]
