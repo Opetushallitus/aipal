f037915bd7e79e00b1cb35e880a5ec05b7e061f9
Antti Virtanen <antti.virtanen@iki.fi>
2016-05-20 11:58:13 +0300

    OPH-1745 ketjutetun raportin käsittely. Ei vielä toimiva versio.

--- aipal/test/clj/aipal/rest_api/raportti/valtakunnallinen_test.clj
+++ aipal/test/clj/aipal/rest_api/raportti/valtakunnallinen_test.clj
@@ -26,12 +26,20 @@
        
 (def kehitysraportti-ketjutettu-json
   (str "{\"kieli\":\"fi\",\"tyyppi\":\"kehitys-ketjutettu\",\"tutkintorakennetaso\":\"tutkinto\",\"koulutusalat\":[],\"opintoalat\":[],"
-       "\"tutkinnot\":[\"X00002\",\"X00001\"],"
+       "\"tutkinnot\":[\"X00001\",\"X00002\"],"
        "\"koulutuksen_jarjestajat\":[],\"jarjestavat_oppilaitokset\":[],\"koulutustoimijat\":[],"
        "\"oppilaitokset\":[],\"taustakysymysryhmaid\":\"3341885\",\"kysymykset\":{\"7312027\":{\"monivalinnat\":{}},\"7312028\":"
        "{\"monivalinnat\":{}},\"7312029\":{\"monivalinnat\":{}},\"7312030\":{\"monivalinnat\":{}},\"7312031\":{\"monivalinnat\":{}},\"7312032\":"
        "{\"monivalinnat\":{}},\"7312033\":{\"monivalinnat\":{}},\"7312039\":{\"monivalinnat\":{}}}}"))
-       
+
+(def kehitysraportti-ei-vastaajia-json
+  (str "{\"kieli\":\"fi\",\"tyyppi\":\"kehitys\",\"tutkintorakennetaso\":\"tutkinto\",\"koulutusalat\":[],\"opintoalat\":[],"
+       "\"tutkinnot\":[\"344102\"],"
+       "\"koulutuksen_jarjestajat\":[],\"jarjestavat_oppilaitokset\":[],\"koulutustoimijat\":[],"
+       "\"oppilaitokset\":[],\"taustakysymysryhmaid\":\"3341885\",\"kysymykset\":{\"7312027\":{\"monivalinnat\":{}},\"7312028\":"
+       "{\"monivalinnat\":{}},\"7312029\":{\"monivalinnat\":{}},\"7312030\":{\"monivalinnat\":{}},\"7312031\":{\"monivalinnat\":{}},\"7312032\":"
+       "{\"monivalinnat\":{}},\"7312033\":{\"monivalinnat\":{}},\"7312039\":{\"monivalinnat\":{}}}}"))
+
 (defn poista-luontipvm-kentat [raportti]
   (clojure.walk/postwalk #(if (map? %) (dissoc % :luontipvm) %) raportti))
 
@@ -41,25 +49,30 @@
                    (peridot/request "/api/raportti/valtakunnallinen"
                                     :request-method :post
                                     :body input-json)
-                   :response)
-        vastaus (body-json response)]
+                   :response)]
     (is (= (:status response) 200))
-    (let [oikea-raportti (clojure.edn/read-string (slurp output-file))]
-      ; (spit "filetto" (with-out-str (clojure.pprint/pprint vastaus)))
-      (is (= (poista-luontipvm-kentat oikea-raportti) (poista-luontipvm-kentat vastaus))))))
+    (let [_ (println "response " response)
+          vastaus (body-json response)]
+      (let [oikea-raportti (clojure.edn/read-string (slurp output-file))]
+       ; (spit "filetto" (with-out-str (clojure.pprint/pprint vastaus)))
+        (is (= (poista-luontipvm-kentat oikea-raportti) (poista-luontipvm-kentat vastaus)))))))
  
 
 (deftest ^:integraatio muodosta-vertailuraportti
   (testing "vertailuraportin perustapaus"
-    (tarkista-valtakunnallinen-raportti perustapaus-json "test/resources/vertailuraportin-perustapaus.edn")))
+    (tarkista-valtakunnallinen-raportti perustapaus-json "test-resources/vertailuraportin-perustapaus.edn")))
       
 (deftest ^:integraatio muodosta-kehitysraportti
   (testing "Kehitysraportin perustapaus"
-    (tarkista-valtakunnallinen-raportti kehitysraportti-json "test/resources/kehitysraportin-perustapaus.edn")))
+    (tarkista-valtakunnallinen-raportti kehitysraportti-json "test-resources/kehitysraportin-perustapaus.edn")))
 
-;(deftest ^:integraatio muodosta-ketjutettu-kehitysraportti
-;  (testing "Kehitysraportin perustapaus"
-;    (tarkista-valtakunnallinen-raportti kehitysraportti-ketjutettu-json "test/resources/kehitysraportin-perustapaus.edn")))
+(deftest ^:integraatio muodosta-kehitysraportti-ei-vastaajia
+  (testing "Kehitysraportin perustapaus"
+    (tarkista-valtakunnallinen-raportti kehitysraportti-ei-vastaajia-json "test-resources/kehitysraportti-eivastaajia.edn")))
+
+(deftest ^:integraatio muodosta-ketjutettu-kehitysraportti
+  (testing "Kehitysraportin ketjuttaminen"
+    (tarkista-valtakunnallinen-raportti kehitysraportti-ketjutettu-json "test-resources/kehitysraportin-perustapaus.edn")))
 
 (deftest ^:integraatio muodosta-tutkintovertailun-parametrit-test
   (are [opintoalat koulutusalat odotettu-tulos]
