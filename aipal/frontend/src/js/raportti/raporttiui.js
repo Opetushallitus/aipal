c876fdeb287b83a6dae540b009aa2b9a05924151
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-05-25 11:38:31 +0300

    ARVO-87: muutetaan paivamaarat javascriptin date muotoon kyselyvalilehdelle ja kyselykertavalilehdelle siirryttaessa

--- aipal/frontend/src/js/raportti/raporttiui.js
+++ aipal/frontend/src/js/raportti/raporttiui.js
@@ -301,7 +301,7 @@ angular.module('raportti.raporttiui', ['ngRoute', 'rest.raportti', 'rest.tutkint
     $scope.raporttiIndeksit = kaavioApurit.raporttiIndeksit;
   }])
 
-  .factory('kyselyValilehti', ['$filter', 'i18n', 'ilmoitus', 'Kysely', 'Raportti', 'raporttiApurit', 'seuranta', function($filter, i18n, ilmoitus, Kysely, Raportti, raporttiApurit, seuranta) {
+  .factory('kyselyValilehti', ['$filter', 'i18n', 'ilmoitus', 'Kysely', 'Raportti', 'raporttiApurit', 'seuranta', 'pvm', function($filter, i18n, ilmoitus, Kysely, Raportti, raporttiApurit, seuranta, pvm) {
     return {
       alusta: function alusta($scope) {
         var suodataKyselyt = function() {
@@ -335,6 +335,7 @@ angular.module('raportti.raporttiui', ['ngRoute', 'rest.raportti', 'rest.tutkint
         });
 
         $scope.valitseKysely = function(kysely) {
+          pvm.parsePvm(kysely);
           if(!kysely.valittu) {
             raporttiApurit.poistaKyselyValinnat($scope.kyselyt, $scope.raportti);
           }
@@ -369,7 +370,7 @@ angular.module('raportti.raporttiui', ['ngRoute', 'rest.raportti', 'rest.tutkint
     };
   }])
 
-  .factory('kyselykertaValilehti', ['$filter', 'i18n', 'ilmoitus', 'Kyselykerta', 'Raportti', 'raporttiApurit', 'seuranta', function($filter, i18n, ilmoitus, Kyselykerta, Raportti, raporttiApurit, seuranta){
+  .factory('kyselykertaValilehti', ['$filter', 'i18n', 'ilmoitus', 'Kyselykerta', 'Raportti', 'raporttiApurit', 'seuranta', 'pvm', function($filter, i18n, ilmoitus, Kyselykerta, Raportti, raporttiApurit, seuranta, pvm){
     return {
       alusta: function alusta($scope) {
         var suodataKyselykerrat = function() {
@@ -403,6 +404,7 @@ angular.module('raportti.raporttiui', ['ngRoute', 'rest.raportti', 'rest.tutkint
         });
 
         $scope.valitseKyselykerta = function(kyselykerta) {
+          pvm.parsePvm(kyselykerta);
           if (!kyselykerta.valittu) {
             raporttiApurit.poistaKyselykertaValinnat($scope.kyselykerrat, $scope.raportti);
           }
