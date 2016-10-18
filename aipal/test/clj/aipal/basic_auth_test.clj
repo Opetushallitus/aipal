eea899fda6f4fd2ac05d847c5f393967a716ac54
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2016-10-18 13:55:03 +0300

    OPH-1718 Lisätään autorisaatiotestiin testi väärällä salasanalla

--- aipal/test/clj/aipal/basic_auth_test.clj
+++ aipal/test/clj/aipal/basic_auth_test.clj
@@ -23,6 +23,9 @@
       (testing "palauttaa 401-vastauksen jos autentikointi epäonnistuu"
         (let [response (handler {})]
           (is (= 401 (:status response)))
+          (is (.startsWith (get-in response [:headers "www-authenticate"] "") "Basic realm=")))
+        (let [response (handler {:headers {"authorization" "Basic dHVubnVzOnbDpMOkcsOk"}})]
+          (is (= 401 (:status response)))
           (is (.startsWith (get-in response [:headers "www-authenticate"] "") "Basic realm="))))
       (testing "päästää validin pyynnön läpi"
         (is (= ::success (handler {:headers {"authorization" "Basic dHVubnVzOnNhbGFzYW5h"}})))))))
