dc482df11ad6d2a71933ed9ac457e68b8fafd997
Joonas Pesonen <joonas.pesonen@csc.fi>
2016-10-21 14:37:29 +0300

    ARVO-118: päivititetään vipunen-rajapinta arvon kontekstiin

--- aipal/src/clj/aipal/toimiala/vipunen.clj
+++ aipal/src/clj/aipal/toimiala/vipunen.clj
@@ -15,34 +15,21 @@
 (ns aipal.toimiala.vipunen
   (:require [schema.core :as s]))
  
-(def TK_sukupuoli (s/enum "nainen" "mies"))
-(def TK_kieli (s/enum "suomi" "ruotsi" "muu"))
-(def TK_ika (s/enum "19 tai alle" "20 - 24" "25 - 34" "35 - 44" "45 - 54" "55 tai yli"))
+(def TK_sukupuoli (s/enum "Nainen" "Mies" "En halua vastata"))
+(def TK_ika (s/enum "Alle 25 vuotta" "25-34 vuotta" "35-45 vuotta" "Yli 45 vuotta"))
 
-(def TK_tutkinto (s/enum "Peruskoulu, keskikoulu tai vastaava"
-                   "Lukio/ylioppilastutkinto"
-                   "Ammatillinen tutkinto tai opistoasteen tutkinto"
-                   "Ammattikorkeakoulututkinto"
-                   "Yliopistotutkinto"
+(def TK_tutkinto (s/enum "Ylioppilastutkinto"
+                   "Ammatillinen perustutkinto/ammattitutkinto"
+                   "Ylioppilastutkinto sekä ammatillinen perustutkinto/ammattitutkinto"
+                   "Opistoasteen tai ammatillisen korkea-asteen tutkinto"
+                   "Korkeakoulututkinto"
                    "Ulkomailla suoritettu tutkinto"
-                   "Ei tutkintoa"))
-
-(def TK_syy (s/enum "Ammatin hankkiminen"
-              "Ammatin vaihtaminen"
-              "Ammattitaidon kehittäminen"
-              "Muu peruste"
-              ))
-
-(def TK_asema (s/enum "Työssä toisen palveluksessa"
-                "Yrittäjä tai itsenäinen ammatinharjoittaja"
-                "Työtön"
-                "Opiskelija"
-                "Muu tilanne (esimerkiksi varusmies-/siviilipalveluksessa,vanhempainvapaalla, kuntoutuksessa tai muu syy)"))
-
-(def TK_tavoite (s/enum "koko tutkinnon suorittaminen"
-                  "tutkinnon osan tai osien suorittaminen"
-                  ))
-
+                   "Ei peruskoulun jälkeistä tutkintoa"
+                   "Ammattikorkeakoulututkinto"
+                   "Alempi korkeakoulututkinto"
+                   "Ylempi korkeakoulututkinto"
+                   "Ulkomailla suoritettu tutkinto"
+                   "Muu"))
 
 (def TK_vastaustyyppi (s/enum
                         "likert_asteikko"
@@ -54,7 +41,6 @@
 (s/defschema VastauksenTiedot {:vastausid s/Int
                                :monivalintavaihtoehto (s/maybe s/Str)
                                :kysely_sv (s/maybe s/Str)
-                               :taustakysymys_aiempi_tilanne (s/maybe TK_asema)
                                :kysymysryhma_sv (s/maybe s/Str)
                                :opintoala_sv (s/maybe s/Str)
                                :vaihtoehto (s/maybe s/Int)
@@ -68,11 +54,9 @@
                                :kysymysryhmaid s/Int
                                :koulutustoimija_sv (s/maybe s/Str)
                                :taustakysymys_sukupuoli TK_sukupuoli
-                               :taustakysymys_tavoite (s/maybe TK_tavoite)
                                :valmistavan_koulutuksen_jarjestaja (s/maybe s/Str)
                                :vastaajaid s/Int
                                :opintoala_fi (s/maybe s/Str)
-                               :taustakysymys_tuleva_tilanne (s/maybe s/Str) ; TODO: enum
                                :vastausaika org.joda.time.LocalDate
                                :koulutustoimija_fi s/Str
                                :rahoitusmuoto s/Str
@@ -81,7 +65,7 @@
                                :kysymysid s/Int
                                :kyselykertaid s/Int
                                :koulutustoimija s/Str
-                               :taustakysymys_tutkinto TK_tutkinto
+                               :taustakysymys_pohjakoulutus TK_tutkinto
                                :vastaustyyppi TK_vastaustyyppi
                                :tutkinto_sv (s/maybe s/Str)
                                :tutkintotunnus (s/maybe s/Str)
@@ -89,10 +73,8 @@
                                :opintoalatunnus (s/maybe s/Str)
                                :valmistavan_koulutuksen_jarjestaja_fi (s/maybe s/Str)
                                :numerovalinta (s/maybe s/Int)
-                               :taustakysymys_aidinkieli TK_kieli
                                :kysymys_sv s/Str
                                :valtakunnallinen s/Bool
                                :kysymys_fi s/Str
                                :kyselyid s/Int
-                               :taustakysymys_syy TK_syy
                                })
