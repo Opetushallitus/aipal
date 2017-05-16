7a047459753243a9f3cbd09293118f0e53cef19c
Jari Asp <jari.asp@solita.fi>
2017-05-16 17:01:15 +0300

    Calling Cheshire's "generate-string" with "escape-non-ascii" option to ensure correct functioning also in Windows dev machines.

--- aipal/test/clj/aipal/rest_api/rest_util.clj
+++ aipal/test/clj/aipal/rest_api/rest_util.clj
@@ -48,3 +48,5 @@ lopuksi. Soveltuu yksinkertaisiin testitapauksiin."
     (cheshire/parse-string (:body response) true)
     (cheshire/parse-string (slurp (:body response) :encoding "UTF-8") true)))
 
+(defn generate-escaped-json-string [form]
+  (cheshire/generate-string form {:escape-non-ascii true}))
