ed0c9fd33b4aa7fbd58b4a31253e72b90c131ed9
Mikko Tyrväinen <mikko.tyrvainen@viklo.fi>
2017-07-03 13:05:33 +0300

    Poistettu käyttämätön dependency

--- aipal/project.clj
+++ aipal/project.clj
@@ -36,7 +36,6 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
-                 [org.flatland/useful "0.11.5"]
                  [org.slf4j/slf4j-api "1.7.16"]
                  [peridot "0.4.3"]
                  [prismatic/schema "1.0.5"]
