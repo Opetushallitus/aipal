931fce5457036a53fa4601e3748ff13202b2e714
Jari Asp <jari.asp@solita.fi>
2017-08-23 10:33:07 +0300

    Lisätty Clojure-utilsin test-path projektin test-pathiksi

--- aipal/project.clj
+++ aipal/project.clj
@@ -57,7 +57,7 @@
   :source-paths ["src/clj" "clojure-utils/src/clj"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7"]
-  :test-paths ["test/clj"]
+  :test-paths ["test/clj" "clojure-utils/test/clj"]
   :test-selectors {:default  (complement (some-fn :integraatio :performance))
                    :integraatio (complement (some-fn :performance))
                    :performance :performance}
