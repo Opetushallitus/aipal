767bd14ec7153dc97fdf688443b9687cdb70082f
Jari Asp <jari.asp@solita.fi>
2017-10-06 15:50:38 +0300

    OPH-1985 Kyselylomakkeen nimen korjaus migraatioskriptillä

---
+++ aipal-db/resources/db/migration/V38_0__kyselyn_nimen_korjaus.sql
@@ -0,0 +1,4 @@
+-- https://issues.solita.fi/browse/OPH-1985
+UPDATE kysely 
+SET nimi_fi = 'Päiväryhmä: Palautekysely tarvittavan ammattitaidon hankkimisesta'
+WHERE nimi_fi = 'Päiväryhmä 2015-2017: Palautekysely tarvittavan ammattitaidon hankkimisesta'
