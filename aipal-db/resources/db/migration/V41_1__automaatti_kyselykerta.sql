84e6a4fd1c05a3fec481c768d1104dfdb71f6cf7
Mikko Tyrväinen <mikko.tyrvainen@viklo.fi>
2018-01-17 16:56:44 +0200

    Tietokantamigraation korjaus.

--- aipal-db/resources/db/migration/V41_1__automaatti_kyselykerta.sql
+++ aipal-db/resources/db/migration/V41_1__automaatti_kyselykerta.sql
@@ -2,6 +2,6 @@ ALTER TABLE kyselykerta ADD COLUMN automaattinen BOOLEAN DEFAULT FALSE;
 
 SET aipal.kayttaja = 'JARJESTELMA';
 
-UPDATE kyselykerta SET automaattinen = TRUE WHERE kyselykertaid = 491;
+UPDATE kyselykerta SET automaattinen = TRUE WHERE WHERE nimi LIKE 'AUTOMAATTI%2018';
 
 UPDATE kyselykerta SET kategoria = '{"vuosi": 2018}' WHERE nimi LIKE 'AUTOMAATTI%2018';
