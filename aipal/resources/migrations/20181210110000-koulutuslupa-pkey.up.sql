919c4fe3cb91b5c92ff6d866e99df8b1c1e59a8f
Mikko Tyrväinen <mikko.tyrvainen@visma.com>
2018-12-10 11:38:50 +0200

    Koulutuslupa-taulun primary key muutos.

---
+++ aipal/resources/migrations/20181210110000-koulutuslupa-pkey.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE koulutustoimija_ja_tutkinto DROP CONSTRAINT koulutustoimija_ja_tutkinto_pkey;
+ALTER TABLE koulutustoimija_ja_tutkinto ADD CONSTRAINT koulutustoimija_ja_tutkinto_pkey PRIMARY KEY (koulutustoimija, tutkinto, voimassa_alkupvm);
