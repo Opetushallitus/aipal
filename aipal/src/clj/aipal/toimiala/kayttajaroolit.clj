771898c8717bc57532766e9783d7a7ef6d8809e2
Mikko Tyrväinen <mikko.tyrvainen@visma.com>
2021-06-04 16:35:01 +0300

    Korjattu ylläpitäjälle näkyvä itserviointi-kyselytyyppi

--- aipal/src/clj/aipal/toimiala/kayttajaroolit.clj
+++ aipal/src/clj/aipal/toimiala/kayttajaroolit.clj
@@ -3,7 +3,7 @@
 (def kyselytyypit {:amk        ["avop" "amk-uraseuranta" "rekrykysely"]
                    :yo         ["kandipalaute" "yo-uraseuranta" "rekrykysely"]
                    :amis       ["amispalaute" "tyoelamapalaute"]
-                   :yllapitaja ["itsearvionti" "ohjaus" "move" "tyoelamapalaute"]})
+                   :yllapitaja ["itsearviointi" "ohjaus" "move" "tyoelamapalaute"]})
 
 (def yllapito-kyselytyypit (distinct (apply concat (vals kyselytyypit))))
 
